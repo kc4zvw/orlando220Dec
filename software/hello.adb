1548994dbd94d107938f04e5a143e0620dc194de
David Billsbrough <billsbrough@gmail.com>
2024-12-17 06:17:24 -0500

    added Ada files and formatting

---
+++ software/hello.adb
@@ -0,0 +1,17 @@
+-- 
+--  $Id: hello.adb,v 0.3 2024/08/10 18:54:19 kc4zvw Exp kc4zvw $
+--
+
+-- (***  Nothing new here but it still had to be done ***)
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Hello is
+begin
+	Put_Line ("");
+	Put_Line ("Hello World from another legacy language!");
+	Put_Line ("");
+end Hello; 
+
+-- ***** End of File *****
